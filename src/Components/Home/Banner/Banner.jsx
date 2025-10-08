f0016cfc46151a4efac54f47316ac3f84456d816
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-10-08 19:17:21 +0600

    added banner botom

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerHeader from './BannerHeader/BannerHeader';
 import BannerImages from './BannerImages/BannerImages';
+import BannerBotom from './BannerBotom/BannerBotom';
 
 const Banner = () => {
     return (
         <div>
             <BannerHeader></BannerHeader>
             <BannerImages></BannerImages>
+            <BannerBotom></BannerBotom>
         </div>
     );
 };
---
+++ src/Components/Home/Banner/BannerBotom/BannerBotom.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const BannerBotom = () => {
+    return (
+        <div className='p-20 bg-linear-65 from-[#632EE3] to-[#9F62F2]'>
+            <h1 className='font-bold text-3xl mb-10 text-center'>Trusted by Millions, Built for You</h1>
+            <div className='flex justify-between'>
+                <div className='py-4 text-center'>
+                    <p>Total Downloads</p>
+                    <h1 className='font-bold text-5xl'>29.6M</h1>
+                    <p>21% more than last month</p>
+                </div>
+                <div className='py-4 text-center'>
+                    <p>Total Reviews</p>
+                    <h1 className='font-bold text-5xl'>906K</h1>
+                    <p>46% more than last month</p>
+                </div>
+                <div className='py-4 text-center'>
+                    <p>Active Apps</p>
+                    <h1 className='font-bold text-5xl'>132+</h1>
+                    <p>31 more will Launch</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerBotom;
