b069947ddd6fbda78db1eb0f2c0c890a1952a9b7
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-10-09 21:46:48 +0600

    improve

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import Chart from '../Chart/Chart';
 
 const About = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
     console.log(id);
     const appId = parseInt(id);
 
@@ -17,13 +18,22 @@ const About = () => {
 
 
     return (
-        <div className='w-11/12 mx-auto'>
-            <div className='grid grid-cols-4 gap-10'>
-                <img src={singleApp.image} alt="" />
-                <div className='col-span-3'>
-                    <h1>{singleApp.title}</h1>
-                    <p>Developed by <span>{singleApp.companyName}</span></p>
-                    <div className='border my-8'></div>
+        <div>
+            <div className='w-11/12 mx-auto my-20'>
+                <div className='grid grid-cols-4 gap-10'>
+                    <img className='w-full h-full' src={singleApp.image} alt="" />
+                    <div className='col-span-3'>
+                        <h1>{singleApp.title}</h1>
+                        <p>Developed by <span>{singleApp.companyName}</span></p>
+                        <div className='border-b my-8'></div>
+                    </div>
+                </div>
+                <div className='border-b my-10'></div>
+                <div>
+                    <Chart singleApp={singleApp}></Chart>
+                    <div className='border-b my-10'></div>
+                    <h1 className='font-bold text-2xl'>Description</h1>
+                    <p className='mt-6'>{singleApp.description}</p>
                 </div>
             </div>
         </div>
---
+++ src/Components/Chart/Chart.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+  ResponsiveContainer
+} from "recharts";
+
+const Chart = ({ singleApp }) => {
+  const reversedRatings = [...singleApp.ratings].reverse();
+
+  return (
+    <div style={{ width: "100%", height: 400 }}>
+      <ResponsiveContainer>
+        <BarChart
+          data={reversedRatings}
+          layout="vertical"
+          margin={{ top: 20, right: 30, left: 80, bottom: 20 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="number" />
+          <YAxis type="category" dataKey="name" />
+          <Tooltip formatter={(value) => value.toLocaleString()} />
+          <Legend />
+          <Bar dataKey="count" fill="#FF8811" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
